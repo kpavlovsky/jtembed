3b32d8e8a62fcff63b98aae6bf5102435d92068f
Kostja P <konstantin.pavlovsky@gmail.com>
2021-06-13 16:52:25 +0200

    updated styles to include hidden

--- jtformtw.js
+++ jtformtw.js
@@ -838,7 +838,7 @@ svg {
                         Name
                     </label>
                     <div class="mt-1">
-                        <input id="jtNameInput" name="text" type="text" autocomplete="name" required
+                        <input id="jtNameInput" name="name" type="text" autocomplete="name" required
                                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                     </div>
                 </div>
